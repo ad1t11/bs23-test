97056593541b385808be88b00338ccf6f668a114
ad1t11 <99913816+ad1t11@users.noreply.github.com>
2022-02-18 02:02:27 +0600

    Updated email fields

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ from select_blue import SelectBlue
 from checkout import Checkout
 
 driver = webdriver.Chrome("E:\projects\python\AD\chromedriver")
+email_1 = 'test1fshgyf5uud5@gmail.com'
+email_2 = 'test1fshgyf6uud6@gmail.com'
 
 def main():
 	driver.get("http://automationpractice.com/index.php")
 	sign_out_obj = SignOut()
 
 	sign_up_page = SignUp()
-	sign_up_page.sign_up('test1fshgyf55@gmail.com',driver)
+	sign_up_page.sign_up(email_1,driver)
 
 	sign_out_obj.sign_out(driver)
 
 	time.sleep(3)
-	sign_up_page.sign_up('test1sdddsdf55@gmail.com',driver)
+	sign_up_page.sign_up(email_2,driver)
 	sign_out_obj.sign_out(driver)
 
 	time.sleep(3)
 	sign_in_page = SignIn()
-	sign_in_page.sign_in(driver, 'test1shersdf55@gmail.com','json123')
+	sign_in_page.sign_in(driver, email_1,'json123')
 	time.sleep(3)
 
 	casual_page = Casual()
